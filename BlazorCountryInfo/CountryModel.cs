0fadc4162e66451df7046bb1f932e80c6d82281d
Maciej <mitek.chmielewski@gmail.com>
2020-07-03 19:40:11 +0200

    Initial UI, requests working

---
+++ BlazorCountryInfo/CountryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorCountryInfo
+{
+  public class CountryModel
+  {
+    public string Name { get; set; }
+    public List<Dictionary<string, string>> Currencies { get; set; }
+    public decimal Population { get; set; }
+  }
+}
