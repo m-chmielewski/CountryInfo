e5f41d201a048d09436fae185348a4ee7b476be2
Maciej <mitek.chmielewski@gmail.com>
2020-07-04 06:31:26 +0200

    Initial UI for customization added

--- BlazorCountryInfo/CountryModel.cs
+++ BlazorCountryInfo/CountryModel.cs
@@ -8,5 +8,13 @@ namespace BlazorCountryInfo
     public string Name { get; set; }
     public List<Dictionary<string, string>> Currencies { get; set; }
     public decimal Population { get; set; }
+    public string Alpha2Code { get; set; }
+    public string Flag
+    {
+      get
+      {
+        return $"https://www.countryflags.io/{ Alpha2Code }/flat/64.png";
+      }
+    }
   }
 }
