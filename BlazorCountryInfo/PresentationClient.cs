8b15f1f7e0607779702a8f2849fbcfe9fec75363
Maciej <mitek.chmielewski@gmail.com>
2020-07-05 13:54:25 +0200

    Working customization

--- BlazorCountryInfo/PresentationClient.cs
+++ BlazorCountryInfo/PresentationClient.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace BlazorCountryInfo
 {
   public class PresentationClient
   {
-    public PresentationClient()
+    public Dictionary<string, bool> FieldsToPresent = new Dictionary<string, bool>
     {
+      { ApiFieldNames.Currencies, false },
+      { ApiFieldNames.Population, true },
+      { ApiFieldNames.Domain, false },
+      { ApiFieldNames.CallingCode, false },
+      { ApiFieldNames.Capital, true },
+      { ApiFieldNames.TimeZones, false },
+      { ApiFieldNames.Region, false },
+      { ApiFieldNames.NativeName, false }
+  };
+
+    public string GetDisplayParameter(bool isVisible)
+    {
+      if (isVisible)
+        return "block";
+
+      return "none";
     }
   }
 }
